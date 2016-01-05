d3ba5ba81182fbc120e330740a4a41cff102a886
kazuma ieiri <kazuma.ieiri@gmail.com>
2016-01-05 22:08:27 +0900

    updated 005.py

--- chapter1/005.py
+++ chapter1/005.py
@@ -1,15 +1,12 @@
 # 05. n-gram
 # 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
 
-def char_ngram(s, n):
-    return [s[i:i+n] for i in range(len(s)-n+1)]
-
-def word_ngram(s, n):
-    words = [word.strip(".,") for word in s.split()]
-    return tuple([words[i:i+n] for i in range(len(words)-n+1)])  
+def n_gram(s, n):
+    words = s
+    return {tuple(words[i:i+n]) for i in range(len(words)-n+1)}
 
 s = "I am an NLPer I am an NLPer"
 
-print(char_ngram(s, 2))
-print(word_ngram(s, 2))
+print(n_gram(s, 2))
+print(n_gram([t.strip(".,") for t in s.split()], 2))
 
